450f802cc0cacba0a57a6022c1519f2c3b77717e
mistertandon <enggparveshtandon@gmail.com>
2017-12-18 14:10:02 +0530

    Developed hello world spring boot application using spring initializer.

---
+++ StudentSpringBootApp/src/main/java/com/mistertandon/app/controller/StudentSpringBootAppController.java
@@ -0,0 +1,17 @@
+package com.mistertandon.app.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StudentSpringBootAppController {
+
+	@GetMapping(value = "/")
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String hello() {
+
+		return "Hello World!!!!";
+	}
+}
