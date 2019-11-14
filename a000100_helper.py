cc3570fccdabaf295ab53dd443dbda94c9de9a64
mistertandon <enggparveshtandon@gmail.com>
2019-11-14 21:19:26 +0530

    refactor: updated 00610_breadth_first_search.ipynb, a000100_helper.py, a00400_binary_heap.ipynb, a00600_graph_adjacency_list.ipynb

--- a000100_helper.py
+++ a000100_helper.py
@@ -12,7 +12,8 @@ def print_object(obj,
                  show_static=True,
                  show_protected=True,
                  seq_length=20,
-                 depth=10):
+                 depth=10,
+                width=40):
     
     print(ppretty(obj, show_address=show_address, indent=indent,
               show_properties=show_properties, show_static=show_static, show_protected=show_protected, seq_length=seq_length, depth=depth))
